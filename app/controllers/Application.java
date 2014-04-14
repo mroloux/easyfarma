5ccd241367d5a11abad6e02c2fca82cd293d7640
Matti Roloux <matti.roloux@gmail.com>
2014-04-14 17:40:10 +0200

    Added form submit handlers

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,32 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import java.util.*;
-
-import models.*;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import play.libs.Mail;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
+    }
+
+    public static void houMeOpDeHoogte(String name, String email) throws EmailException {
+        sendEmail("Hou me op de hoogte formulier ingevuld", name + " - " + email);
+    }
+
+    public static void contact(String name, String email, String message) throws EmailException {
+        sendEmail("Contact formulier ingevuld", name + " - " + email + " - " + message);
+    }
+
+    private static void sendEmail(String subject, String body) throws EmailException {
+        SimpleEmail simpleEmail = new SimpleEmail();
+        simpleEmail.setFrom("info@easyfarma.be");
+        simpleEmail.addTo("matti@easyfarma.be");
+        simpleEmail.setSubject(subject);
+        simpleEmail.setMsg(body);
+        Mail.send(simpleEmail);
+        renderText("Thanks pal!");
     }
 
 }
--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,81 +1,44 @@
-// JavaScript Document
-(function(){
-  "use strict";
-//Banner height
-$(document).ready(function() {
+$(function () {
+    var viewport_width = window.innerWidth;
+    var viewport_height = window.innerHeight;
 
-	var viewport_width = window.innerWidth;
-	var viewport_height = window.innerHeight;
-
-//Banner
-	$('.banner,.carousel .item').height((viewport_height));
-	$(window).resize(function() {
-	  var viewport_width = window.innerWidth;
-	  var viewport_height = window.innerHeight;
-		$('.banner,.carousel .item').height((viewport_height));
-	});
-	});
-	
-//Page Smooth Scrolling
-
-$(document).ready(function() {
-		$('ul.navi a').bind('click',function(event){
-			var $anchor = $(this);
-	
-			$('html, body').stop().animate({
-			scrollTop:( $($anchor.attr('href')).offset().top-0) 
-			}, 1500,'easeInOutExpo');
-                    /*
-                    if you don't want to use the easing effects:
-                    $('html, body').stop().animate({
-                        scrollTop: $($anchor.attr('href')).offset().top
-                    }, 1000);
-                    */
-					event.preventDefault();
-					});
-			});
-
-//Opt-in Form
-
-$(document).ready(function(){
- 
-$('#submit').click(function(){
-
-$.post("send.php", $("#contactform").serialize(),  function(response) {
-$('#message').html(response);
-$( "#message" ).show(1000);
-});
-return false;
- 
-});	
+    $('.banner,.carousel .item').height((viewport_height));
+    $(window).resize(function () {
+        var viewport_width = window.innerWidth;
+        var viewport_height = window.innerHeight;
+        $('.banner,.carousel .item').height((viewport_height));
+    });
 });
 
-//Contact Form
+$(function () {
+    $('ul.navi a').bind('click', function (event) {
+        var $anchor = $(this);
 
-$(document).ready(function(){
- 
-$('#sendmsg').click(function(){
-
-$.post("contact.php", $("#contactmsg").serialize(),  function(response) {
-$('#contact-message').html(response);
-$( "#contact-message" ).show(1000);
+        $('html, body').stop().animate({
+            scrollTop: ( $($anchor.attr('href')).offset().top - 0)
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
 });
-return false;
- 
-});	
-});			
-//Flexslider for testimonials
-	// Can also be used with $(document).ready()
-	$(window).load(function() {
-	 
-	  $('#testimonial').flexslider({
-		animation: "slide",
-		controlNav: true,
-		directionNav:false,
-		animationLoop: true,
-		slideshow: false,
-	
-	  });
-	});
 
-		})();
+$(function () {
+    $('#contactmsg').submit(function () {
+        $.post('/contact', $(this).serialize())
+            .success(function () {
+                $('#contactmsg').slideUp();
+                $('#contact-message').slideDown();
+            });
+        return false;
+    })
+});
+
+$(function () {
+    $('#contactform').submit(function () {
+        $.post('/houmeopdehoogte', $(this).serialize())
+            .success(function () {
+                $('#contactform').slideUp();
+                $('#houmeopdehoogte-message').slideDown();
+            });
+        return false;
+    })
+});
